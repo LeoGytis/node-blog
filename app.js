c5bd9ad2039d314bfcbdcee3b1f4d8c295e3e78c
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-21 22:34:26 +0200

    get data from mongoDB

--- app.js
+++ app.js
@@ -69,31 +69,40 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Yoshi finds eggs to bake some cake",
-      id: 1,
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Mario finds stars to save the princess",
-      id: 2,
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "To defeat bowser, you need to find the secret axe",
-      id: 3,
-    },
-  ];
-  res.render("index", {
-    title: "Home",
-    blogs,
-  });
+  res.redirect("/blogs");
+  //   const blogs = [
+  //     {
+  //       title: "Yoshi finds eggs",
+  //       snippet: "Yoshi finds eggs to bake some cake",
+  //     },
+  //     {
+  //       title: "Mario finds stars",
+  //       snippet: "Mario finds stars to save the princess",
+  //     },
+  //     {
+  //       title: "How to defeat bowser",
+  //       snippet: "To defeat bowser, you need to find the secret axe",
+  //     },
+  //   ];
+  //   res.render("index", {
+  //     title: "Home",
+  //     blogs,
+  //   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { blogs: result, title: "All blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
