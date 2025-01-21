e54079998809329e55bb60c4f44071273e2690e2
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-21 15:11:42 +0200

    creation

--- server.js
+++ server.js
@@ -2,19 +2,34 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  //   console.log("Request received", req);
+  console.log("Request received", req.url, req.method);
 
   //set header content type
   res.setHeader("Content-Type", "text/html");
 
-  fs.readFile("./views/index.html", (err, data) => {
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     } else {
       res.write(data);
-      res.write("<h1>Hello brother from node server</h1>");
-      res.end(data);
+      res.end();
     }
   });
 });
