b13a00aaa4bf85b3237b6caa8242d5e0ddf5d079
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-21 19:16:08 +0200

    ejs system

--- app.js
+++ app.js
@@ -10,18 +10,14 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
-});
-
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
