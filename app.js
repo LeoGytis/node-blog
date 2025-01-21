143eb0b1c851270886329ac73ba2ee9f34c2dd34
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-21 19:37:48 +0200

    save

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { title } = require("process");
 
 const app = express();
 
@@ -7,17 +8,37 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Yoshi finds eggs to bake some cake",
+      id: 1,
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Mario finds stars to save the princess",
+      id: 2,
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "To defeat bowser, you need to find the secret axe",
+      id: 3,
+    },
+  ];
+  res.render("index", {
+    title: "Home",
+    blogs,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create new blog" });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
